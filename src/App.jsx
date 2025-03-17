492851eec8a2bcfd18c70f94577d59e022928800
AlexMan123456 <alex.man03888@gmail.com>
2025-03-17 02:17:03 +0000

    Added song to card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import FrontCardPage from "./components/FrontCardPage"
 import Navigation from "./components/Navigation"
 import MessagePage from "./components/MessagePage"
 import QuizPage from "./components/QuizPage"
+import audio from "./audio/willow.mp3"
+import 'react-h5-audio-player/lib/styles.css'
+import H5AudioPlayer from "react-h5-audio-player"
 
 function App() {
 
@@ -13,6 +16,7 @@ function App() {
       <Route path="/card/2" element={<QuizPage/>}/>
       <Route path="/card/3" element={<MessagePage/>}/>
     </Routes>
+    <H5AudioPlayer style={{width: "30vw"}} src={audio} showJumpControls={false} showSkipControls={false}/>
     <Navigation/>
     </>)
 }
